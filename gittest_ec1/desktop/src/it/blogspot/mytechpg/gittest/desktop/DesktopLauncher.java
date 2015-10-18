c8287e203dfe13f38551a0da7051608a8836b5cd
mytechpg <mytechp@gmail.com>
2015-10-18 14:47:13 +0200

    added gradle output, imported in eclipse

---
+++ gittest_ec1/desktop/src/it/blogspot/mytechpg/gittest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package it.blogspot.mytechpg.gittest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import it.blogspot.mytechpg.gittest.GitTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GitTest(), config);
+	}
+}
